3aaf3f073fe8c476647e1cc66e55806487c0d7e5
Liliia Lyshchuk <liliia.lyshchuk@gmail.com>
2024-03-09 12:52:35 +0200

    Changed logo in header

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "welcome#index" # шукає контролер welcome з екшином(методом) index
-  get 'home', to: 'welcome#index'
   get 'about', to: 'welcome#about'
   get 'biking', to: 'places#biking'
   get 'climbing', to: 'places#climbing'
