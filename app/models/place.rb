8e2d4e0932f0ae9c6474748da858d5643c1037b0
Liliia Lyshchuk <liliia.lyshchuk@gmail.com>
2024-03-18 16:00:46 +0200

    Update active storage

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,12 +6,12 @@ class Place < ApplicationRecord
     validates :preview_img, presence: true
     validates :description, presence: true
 
-    def self.ransackable_associations(auth_object = nil)
-        ["author", "preview_img_attachment", "preview_img_blob"]
-    end
-
     def self.ransackable_attributes(auth_object = nil)
         ["author_id", "created_at", "description", "id", "id_value", "published_at", "updated_at"]
+    end
+
+    def self.ransackable_associations(auth_object = nil)
+        ["author", "preview_img_attachment", "preview_img_blob"]
     end
 end
 
